927d79b9b8fc6f75c926fb510fd96adcdd47d73e
ochiba0227 <newgroundofswim@gmail.com>
2019-05-09 20:03:51 +0900

    FC

---
+++ src/main/kotlin/com/example/learn_kotlin/LearnKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.learn_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearnKotlinApplication>(*args)
+}
